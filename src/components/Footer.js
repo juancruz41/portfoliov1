74a9ca7512fb59daa1af5d132d193ff50112426b
jotace <juancruz041@gmail.com>
2025-03-01 18:30:48 -0300

    footer corregido
    footer corregido

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,22 @@
 import React from "react";
-import { Container, Typography } from "@mui/material";
+import { Container, Typography, useTheme } from "@mui/material";
 
 function Footer() {
+  const theme = useTheme(); // Accede al tema actual
+
   return (
-    <Container sx={{ textAlign: "center", padding: "20px 0", marginTop: "20px", backgroundColor: "#f5f5f5" }}>
-      <Typography variant="body1">Email: juancruz@example.com | Phone: +123 456 7890</Typography>
+    <Container
+      sx={{
+        textAlign: "center",
+        padding: "20px 0",
+        marginTop: "20px",
+        backgroundColor: theme.palette.background.default, // Usa el color de fondo del tema
+        color: theme.palette.text.primary, // Usa el color del texto del tema
+      }}
+    >
+      <Typography variant="body1">
+        Email: juancruz@example.com | Phone: +123 456 7890
+      </Typography>
     </Container>
   );
 }
